dc75fa49036a4b1e193e947821788f9ab9b115d3
kitman <kitman200220022002@gmail.com>
2023-11-26 11:22:15 +1100

    fix db connection

--- src/loaders/mongoose.ts
+++ src/loaders/mongoose.ts
@@ -2,10 +2,11 @@ export {};
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 module.exports = async function () {
-  const connection = await mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  // const connection = await mongoose.connect(process.env.MONGODB_URL, {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  // });
 
-  return connection.connection.db;
+  // return connection.connection.db;
+  return null;
 };
